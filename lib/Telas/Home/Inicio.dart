e84415aac2b07cdb1de01832b1973687d27ac84a
barreira <barreira@localhost.localdomain>
2019-12-27 21:46:25 -0500

    JWT

--- lib/Telas/Home/Inicio.dart
+++ lib/Telas/Home/Inicio.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../Funcoes/UserData.dart';
-
+import '../Parcial/Projetos.dart';
 
 // var jwt  = void_getJWT();
 // print(jwt);
@@ -12,45 +12,24 @@ class InicioState extends StatefulWidget {
 class Inicio extends State<InicioState> {
   String username = '';
 
+  @override
+  void initState() {
 
-@override
-  void initState(){
-    super.initState();  
-    getUser()
-      .then(( username ) {
-        setState(() {
-          this.username = username;
-        });
+    super.initState();
+    getUser().then((username) {
+      setState(() {
+        this.username = username;
       });
+    });
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // final title = 'Itens';
 
     return MaterialApp(
-      // title: title,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: Text(title),
-        // ),
-        body: ListView(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Text("Bem vindo, " + this.username + " !")
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
-      ),
+      home: Projetos()
     );
   }
 }
